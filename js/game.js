183820ea384f6522e0d4ecbcfe9e5003aec01cd2
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-26 16:59:20 +0000

    Initial commit

---
+++ js/game.js
@@ -0,0 +1,51 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Evgeny Vovk All rights reserved
+//
+// Created by: Evgeny Vovk
+// Created on: May 2022
+// This is the Phaser3 game configuration file
+
+// scene import statements
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+
+// create the new scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+//* Game scene */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    }
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+
+// load scenes
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
+
+// the start scene
+game.scene.start("splashScene")
---
+++ js/gameScene.js
@@ -0,0 +1,94 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Evgeny Vovk All rights reserved
+//
+// Created by: Evgeny Vovk
+// Created on: May 2022
+// This is the Game Scene
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "gameScene" })
+
+    this.background = null
+    this.ship = null
+    this.fireMissile = false
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff")
+  }
+
+  preload() {
+    console.log("Game Scene")
+
+    // images
+    this.load.image("starBackground", "./assets/starBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("missile", "./assets/missile.png")
+  }
+
+  create(data) {
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    //create a group for the missiles
+    this.missileGroup = this.physics.add.group()
+  }
+
+  update(time, delta) {
+    //called 60 times a second.
+
+    const keyForwardObj = this.input.keyboard.addKey("W")
+    const keyLeftObj = this.input.keyboard.addKey("A")
+    const keyBackwardObj = this.input.keyboard.addKey("S")
+    const keyRightObj = this.input.keyboard.addKey("D")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
+    
+
+    if (keyForwardObj.isDown === true) {
+      this.ship.y -= 6
+      if (this.ship.y < 700) {
+        this.ship.y = 700
+      }
+    }
+    
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 6
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+    
+    if (keyBackwardObj.isDown === true) {
+      this.ship.y += 6
+      if (this.ship.y > 1080) {
+        this.ship.y = 1080
+      }
+    }
+    
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 6
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
+    
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        //fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
+        this.missileGroup.add(aNewMissile)
+      }
+    }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
+    }
+  }
+}
+
+export default GameScene
